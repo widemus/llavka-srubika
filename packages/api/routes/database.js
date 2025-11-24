2d49dce47823bd0caf3a399a553490455991c286
widemus <zagrajanton@gmail.com>
2025-11-24 03:36:50 +0000

    ))

--- packages/api/routes/database.js
+++ packages/api/routes/database.js
@@ -8,6 +8,9 @@ async function dbConnector(fastify, options) {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    ssl: {
+      rejectUnauthorized: false
+    }
   };
 
   // Socket Path 
