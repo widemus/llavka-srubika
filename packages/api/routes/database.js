9569aa090e57b07748c0c0df22b46b771c0ccaee
widemus <zagrajanton@gmail.com>
2025-11-24 03:13:44 +0000

    updated connection logic

--- packages/api/routes/database.js
+++ packages/api/routes/database.js
@@ -13,23 +13,6 @@ async function dbConnector(fastify, options) {
     fastify.log.info(`Connecting to Cloud SQL via Socket: ${process.env.DB_SOCKET_PATH}`);
     dbConfig.socketPath = process.env.DB_SOCKET_PATH;
   }
-  // Host/Port
-  else {
-    fastify.log.info(`Connecting to Database via TCP: ${process.env.DB_HOST}`);
-    dbConfig.host = process.env.DB_HOST || '127.0.0.1';
-    dbConfig.port = process.env.DB_PORT || 3306;
-
-    // SSL Configuration
-    if (process.env.DB_SSL === 'true') {
-      fastify.log.info('Enabling SSL for Database Connection');
-      dbConfig.ssl = {
-        rejectUnauthorized: false
-      };
-    }
-  }
-
-  // Debug Log
-  fastify.log.info(`DB Config: Host=${dbConfig.host}, User=${dbConfig.user}, SSL=${!!dbConfig.ssl}`);
 
   try {
     const pool = mysql.createPool(dbConfig);
